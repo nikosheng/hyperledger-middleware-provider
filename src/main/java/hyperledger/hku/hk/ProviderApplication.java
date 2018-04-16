92e27895bcd6d2dfd374205657375e7d8b27c8ff
Niko Feng <nikofeng@connect.hku.hk>
2018-04-16 12:13:30 +0800

    Spring Data Redis merge

--- src/main/java/hyperledger/hku/hk/ProviderApplication.java
+++ src/main/java/hyperledger/hku/hk/ProviderApplication.java
@@ -2,10 +2,12 @@ package hyperledger.hku.hk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableCaching
 public class ProviderApplication
 {
     public static void main( String[] args )
--- src/main/java/hyperledger/hku/hk/controller/HyperledgerController.java
+++ src/main/java/hyperledger/hku/hk/controller/HyperledgerController.java
@@ -1,19 +1,27 @@
 package hyperledger.hku.hk.controller;
 
 import hyperledger.hku.hk.utils.HttpUtils;
+import org.apache.commons.lang.StringUtils;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.util.EntityUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.concurrent.TimeUnit;
 
 @Controller
 public class HyperledgerController {
     private static final String PROTECTED_RESOURCE_URL = "http://hyperledger.hku.hk/api/";
+    public static final String ACCESS_TOKEN = "access_token";
+
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
 
     @RequestMapping("/")
     public String home(HttpServletRequest request, HttpServletResponse response) {
@@ -28,23 +36,32 @@ public class HyperledgerController {
 
     @RequestMapping(value = "/api/{item}", method = RequestMethod.GET)
     @ResponseBody
-    public String application(@RequestParam String item, HttpServletRequest request) throws Exception{
+    public String application(@PathVariable String item, HttpServletRequest request) throws Exception {
         String ret = null;
-        String accessToken = getAccessToken(request.getCookies());
-        if (accessToken != null && accessToken.length() > 0) {
-            String url = String.format("%s%saccess_token=%s", PROTECTED_RESOURCE_URL, item, accessToken);
+        String accessToken = redisTemplate.opsForValue().get(ACCESS_TOKEN);
+        if (StringUtils.isEmpty(accessToken)) {
+            Cookie[] cookies = request.getCookies();
+            if (cookies != null && cookies.length > 0) {
+                accessToken = getAccessToken(cookies);
+            }
+        }
+
+        if (StringUtils.isNotEmpty(accessToken)) {
+            redisTemplate.opsForValue().set(ACCESS_TOKEN, accessToken, 7, TimeUnit.DAYS);
+            String url = String.format("%s%s?access_token=%s", PROTECTED_RESOURCE_URL, item, accessToken);
             HttpResponse response =
                     HttpUtils.get(url);
             HttpEntity entity = response.getEntity();
             ret = EntityUtils.toString(entity);
         }
+
         return ret;
     }
 
     private String getAccessToken(Cookie[] cookies) {
         String accessToken = null;
         for (Cookie cookie : cookies) {
-            if ("access_token".equals(cookie.getName())) {
+            if (ACCESS_TOKEN.equals(cookie.getName())) {
                 String value = cookie.getValue();
                 accessToken = value.substring(4, value.indexOf("."));
             }
